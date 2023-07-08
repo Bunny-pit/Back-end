3b94be3d4501f396b7571cd97164b1f9d7a484f3
ChanghyeonO <ckdgus5189@gmail.com>
2023-07-08 10:25:37 +0900

    fix: 포스트맨 테스트시 MainhomeService.~~ is not a function 나오는 문제 수정

--- src/controllers/Mainhome_controller.js
+++ src/controllers/Mainhome_controller.js
@@ -3,7 +3,7 @@ import MainhomeService from "../services/Mainhome_service.js";
 const MainhomeController = {
   async createMainhomePost(req, res) {
     try {
-      const newPost = await MainhomeService.createPost(req.body);
+      const newPost = await MainhomeService.createMainhomePost(req.body);
 
       res.status(201).json(newPost);
     } catch (error) {
@@ -13,7 +13,7 @@ const MainhomeController = {
 
   async getAllMainhomePosts(req, res) {
     try {
-      const posts = await MainhomeService.getAllPosts();
+      const posts = await MainhomeService.getAllMainhomePosts();
 
       res.json(posts);
     } catch (error) {
@@ -24,7 +24,10 @@ const MainhomeController = {
   async updateMainhomePost(req, res) {
     try {
       const { id } = req.params;
-      const updatedPost = await MainhomeService.updatePost(id, req.body);
+      const updatedPost = await MainhomeService.updateMainhomePost(
+        id,
+        req.body,
+      );
 
       if (!updatedPost) {
         return res.status(404).json({ error: "게시글을 찾지 못했습니다." });
@@ -39,7 +42,7 @@ const MainhomeController = {
   async deleteMainhomePost(req, res) {
     try {
       const { id } = req.params;
-      const deletedPost = await MainhomeService.deletePost(id);
+      const deletedPost = await MainhomeService.deleteMainhomePost(id);
 
       if (!deletedPost) {
         return res.status(404).json({ error: "게시글을 찾지 못했습니다." });
