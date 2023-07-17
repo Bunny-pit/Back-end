0f75fb5b6025365ece3f0808577081227180685a
ryuiseo <rks85227@gmail.com>
2023-07-17 12:56:47 +0900

    Feat : socket io

--- src/controllers/message_controller.js
+++ src/controllers/message_controller.js
@@ -1,14 +1,18 @@
 import MessageService from '../services/message_service.js';
+import { getSocketIo } from '../lib/socket.js';
 
 const MessageController = {
   async createMessage(req, res) {
     try {
       const { senderId, chatId, content } = req.body;
-      const newMessage = await MessageService.createMessage(
+      const newMessage = await MessageService.createMessageAndEmit(
         senderId,
         chatId,
         content,
       );
+      const io = getSocketIo();
+      io.to(chatId).emit('newMessage', newMessage);
+
       res.status(201).json(newMessage);
     } catch (error) {
       res.status(500).json({ error: error.message });
