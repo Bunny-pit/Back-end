98c8e85b265f1928379dfa06f1399d5f8478d2e1
ryuiseo <rks85227@gmail.com>
2023-07-18 18:34:05 +0900

    Fix : app.js

--- app.js
+++ app.js
@@ -21,9 +21,11 @@ const io = initializeSocketIo(server);
 //express 탑재 body-parser 사용, cors 설정
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  credentials : true,
-}));
+app.use(
+  cors({
+    credentials: true,
+  }),
+);
 
 //환경 설정
 dotenv.config();
@@ -40,17 +42,19 @@ app.get('/', (req, res) => {
   res.send('여기는 버니톡의 백엔드 페이지입니다!');
 });
 
-//모든 라우터 코드를 동적으로 불러오도록 설정
-Promise.all(
-  files.map(async (file) => {
-    if (file.endsWith('.js')) {
-      const route = await import(path.join('file://', routesPath, file));
-      const apiEndpoint = '/api/' + file.replace('_router.js', '');
-      app.use(apiEndpoint, route.default);
-    }
-  }),
-).then(() => {
-  app.listen(port, () => {
+const startServer = async () => {
+  // 모든 라우터 코드를 동적으로 불러오도록 설정
+  await Promise.all(
+    files.map(async (file) => {
+      if (file.endsWith('.js')) {
+        const route = await import(path.join('file://', routesPath, file));
+        const apiEndpoint = '/api/' + file.replace('_router.js', '');
+        app.use(apiEndpoint, route.default);
+      }
+    }),
+  );
+
+  server.listen(port, () => {
     logger.info(`서버가 http://localhost:${port}에 성공적으로 연결되었습니다.`);
     mongoose
       .connect(process.env.MONGODB_URI, {
@@ -60,6 +64,7 @@ Promise.all(
       .then(() => logger.info('몽고디비 연결에 성공했습니다.'))
       .catch((e) => console.error(e));
   });
+
   io.on('connection', (socket) => {
     console.log('User connected: ' + socket.id);
 
@@ -86,4 +91,6 @@ Promise.all(
       console.log('User disconnected: ' + socket.id);
     });
   });
-});
+};
+
+startServer();
