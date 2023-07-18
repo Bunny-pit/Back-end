73cfa90e82763844eb97b5ffcacc5a6f59361acb
ryuiseo <rks85227@gmail.com>
2023-07-18 17:16:23 +0900

    Fix : middleware

--- src/routers/user_router.js
+++ src/routers/user_router.js
@@ -1,28 +1,26 @@
-import express from "express";
-import UserController from '../controllers/User_controller.js'
-import { adminCheck, loginRequired } from "../middlewares/index.js";
+import express from 'express';
+import UserController from '../controllers/User_controller.js';
+import { adminCheck, loginRequired } from '../middlewares/index.js';
 
 const userRouter = express.Router();
 
-userRouter.get("/", (req, res) => {
-    res.send('유저 루트 페이지');
-})
+userRouter.get('/', (req, res) => {
+  res.send('유저 루트 페이지');
+});
 
 //유저 기능
-userRouter.post("/register", UserController.createUser);
+userRouter.post('/register', UserController.createUser);
 
-userRouter.post("/login", loginRequired, UserController.loginUser);
-userRouter.post("/logout", loginRequired, UserController.logout);
+userRouter.post('/login', UserController.loginUser);
+userRouter.post('/logout', loginRequired, UserController.logout);
 
-userRouter.patch("/edit", loginRequired, UserController.updateUser);
-userRouter.delete("/delete", UserController.deleteUser);
+userRouter.patch('/edit', loginRequired, UserController.updateUser);
+userRouter.delete('/delete', UserController.deleteUser);
 
-userRouter.get("/accessToken", UserController.loginSuccess);
-userRouter.get("/refreshToken", UserController.refreshToken);
+userRouter.get('/accessToken', UserController.loginSuccess);
+userRouter.get('/refreshToken', UserController.refreshToken);
 
 //관리자 기능
-userRouter.get("/register", adminCheck, UserController.getUser)
-
-
+userRouter.get('/register', adminCheck, UserController.getUser);
 
 export default userRouter;
