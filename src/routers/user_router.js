574d2c0ade43fa52a99139be85753161232653b0
ChanghyeonO <ckdgus5189@gmail.com>
2023-07-23 20:44:13 +0900

    fix: 배포 에러로 인한 파일명 변경

--- src/routers/user_router.js
+++ src/routers/user_router.js
@@ -11,17 +11,16 @@ userRouter.get('/', (req, res) => {
 userRouter.post('/register', UserController.createUser);
 userRouter.get('/register', UserController.getUser);
 
+userRouter.post('/login', UserController.loginUser);
+userRouter.post('/logout', UserController.logout);
 
-userRouter.post("/login", UserController.loginUser);
-userRouter.post("/logout", UserController.logout);
+userRouter.patch('/edit', loginRequired, UserController.updateUser);
+userRouter.delete('/delete', loginRequired, UserController.deleteUser);
 
-userRouter.patch("/edit", loginRequired, UserController.updateUser);
-userRouter.delete("/delete", loginRequired, UserController.deleteUser);
-
-userRouter.get("/accessToken", UserController.accessToken);
+userRouter.get('/accessToken', UserController.accessToken);
 // userRouter.get("/refreshToken", UserController.refreshToken);
 // userRouter.get("/loginsuccess", UserController.loginSuccess);
 //관리자 기능
-userRouter.get("/register", UserController.getUser)
+userRouter.get('/register', UserController.getUser);
 
 export default userRouter;
