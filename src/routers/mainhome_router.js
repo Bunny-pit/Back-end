fb914ae16d516f53cf380ff43ac769be09273e82
ChanghyeonO <ckdgus5189@gmail.com>
2023-07-31 21:59:17 +0900

    fix: 코드 누락으로 인한 커밋

--- src/routers/mainhome_router.js
+++ src/routers/mainhome_router.js
@@ -5,20 +5,20 @@ import loginRequired from '../middlewares/login_required.js';
 const mainhome_router = express.Router();
 
 //게시글 작성
-mainhomeRouter.post('/', loginRequired, MainhomeController.createMainhomePost);
+mainhome_router.post('/', loginRequired, MainhomeController.createMainhomePost);
 
 //게시글 전부 불러오기
-mainhomeRouter.get('/', MainhomeController.getAllMainhomePosts);
+mainhome_router.get('/', MainhomeController.getAllMainhomePosts);
 
 //게시글 수정하기
-mainhomeRouter.patch(
+mainhome_router.patch(
   '/:id',
   loginRequired,
   MainhomeController.updateMainhomePost,
 );
 
 //게시글 삭제하기
-mainhomeRouter.delete(
+mainhome_router.delete(
   '/:id',
   loginRequired,
   MainhomeController.deleteMainhomePost,
