e1b67286ecef529b1ba6c9703488e53458007e64
ryuiseo <rks85227@gmail.com>
2023-08-02 14:29:24 +0900

    socket

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -5,7 +5,8 @@ let io;
 export const initializeSocketIo = (server) => {
   io = new Server(server, {
     cors: {
-      origin: 'https://web-front-end-kvmh2mljxnw03c.sel4.cloudtype.app',
+      // origin: 'https://web-front-end-kvmh2mljxnw03c.sel4.cloudtype.app',
+      origin: 'http://localhost:3001',
       methods: ['GET', 'POST'],
       allowedHeaders: ['my-custom-header'],
       credentials: true,
