3222f1b393e981639f2ad7a1325f945ba3644966
ryuiseo <rks85227@gmail.com>
2023-08-03 11:24:11 +0900

    Fix : url

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -5,8 +5,7 @@ let io;
 export const initializeSocketIo = (server) => {
   io = new Server(server, {
     cors: {
-      // origin: 'https://web-front-end-kvmh2mljxnw03c.sel4.cloudtype.app',
-      origin: 'http://localhost:3001',
+      origin: process.env.ORIGIN || 'http://localhost:3001',
       methods: ['GET', 'POST'],
       allowedHeaders: ['my-custom-header'],
       credentials: true,
