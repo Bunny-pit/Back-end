459cec586aba83c0f2ce69ca50b2f695e894a7c9
ChanghyeonO <ckdgus5189@gmail.com>
2023-08-12 03:14:21 +0900

    feat:게시글 유저 신고, 조회 기능 추가

--- src/controllers/mainhomeFriends_controller.js
+++ src/controllers/mainhomeFriends_controller.js
@@ -71,6 +71,39 @@ const mainhomeFriendsController = {
       res.status(500).json({ error: error.message });
     }
   },
+
+  async reportPost(req, res) {
+    try {
+      const { id: postId } = req.params;
+      const { reason } = req.body;
+
+      const post = await MainhomeFriendsService.reportPost(
+        req.oid,
+        postId,
+        reason,
+      );
+
+      res.json({ message: '게시글을 성공적으로 신고했습니다.', post });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+  async getReportedPosts(req, res) {
+    try {
+      const reportedPosts = await MainhomeFriendsService.getReportedPosts();
+
+      if (!reportedPosts.length) {
+        return res
+          .status(404)
+          .json({ message: '신고 3회 이상 누적된 게시글이 없습니다.' });
+      }
+
+      res.json(reportedPosts);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
 };
 
 export default mainhomeFriendsController;
