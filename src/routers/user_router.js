b84656735bdc1069b7b6d63a702ddc0bf55f25a0
김종현 <plmij1501@gmail.com>
2023-08-12 12:11:17 +0900

    fix:유저 라우터 주석 추가

--- src/routers/user_router.js
+++ src/routers/user_router.js
@@ -8,18 +8,20 @@ userRouter.get('/', (req, res) => {
   res.send('유저 루트 페이지 입니다.');
 });
 
+//유저 기능
 userRouter.post('/register', UserController.createUser);
 userRouter.post('/login', UserController.loginUser);
-userRouter.get('/login', UserController.getAllUser); //모든 유저 조회
 userRouter.post('/logout', UserController.logout);
-userRouter.get('/register', UserController.getUser);
 userRouter.patch('/edit', loginRequired, UserController.updateUser);
 userRouter.delete('/delete', loginRequired, UserController.deleteUser);
-//토큰 값 가져옴.
+
+//토큰 확인 기능
 userRouter.get('/accessToken', UserController.accessToken);
 userRouter.post('/refereshToken', UserController.refreshToken);
-//관리자 기능 추후 활성화
-// userRouter.get('/register', loginRequired, UserController.getUser);
+
+//관리자 기능 
+userRouter.get('/login', UserController.getAllUser); //모든 유저 조회. 개발 편의를 위해 임시 활성화 하였음.
+// userRouter.get('/register', UserController.getUser); //특정 유저 호회
 
 //팔로우 기능
 userRouter.post('/toggleFollow', loginRequired, UserController.toggleFollow);
