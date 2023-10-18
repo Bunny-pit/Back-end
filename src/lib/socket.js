e0fb5559faadbd76ad3be807220e46b8cd0371db
ChanghyeonO <ckdgus5189@gmail.com>
2023-10-19 01:47:01 +0900

    fix: 소켓 통신 경로 수정중

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -3,9 +3,11 @@ import { Server } from 'socket.io';
 let io;
 
 export const initializeSocketIo = server => {
+  const origins = (process.env.ORIGIN || 'http://localhost:3000').split(',');
+
   io = new Server(server, {
     cors: {
-      origin: process.env.ORIGIN || 'http://localhost:3000',
+      origin: origins,
       methods: ['GET', 'POST'],
       allowedHeaders: ['my-custom-header'],
       credentials: true,
